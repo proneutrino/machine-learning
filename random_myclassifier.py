2c529c6e440087541b67fde46d0fa20983da151c
Faheemuddin Rumaan Shaik <32872170+proneutrino@users.noreply.github.com>
2018-05-17 06:28:30 +0530

    Add files via upload

---
+++ random_myclassifier.py
@@ -0,0 +1,43 @@
+import random
+
+
+class newclassifier():
+	def fit(self,X_train,y_train):
+		self.X_train=X_train
+		self.y_train=y_train
+
+	def predict(self,X_test):
+		predictions=[]
+        
+         	for row in X_test:
+        		label = random.choice(self.y_train)
+
+        		predictions.append(label)
+
+	 	return predictions
+    
+
+
+
+     
+
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+y=iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=.5)
+
+
+"""from sklearn import tree"""
+my_classifier = newclassifier()
+
+my_classifier.fit(X_train,y_train)
+
+predictions = my_classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(y_test,predictions)
